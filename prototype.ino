e7c3ba3d04cbcdc00c1b6eaf024b97372cf0a0f6
jcasuga <70498866+jcasuga@users.noreply.github.com>
2022-04-06 15:18:39 -0700

    Create prototype.ino

---
+++ prototype.ino
@@ -0,0 +1,41 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <RTCZero.h>
+#include <LiquidCrystal.h>
+#include <Stepper.h>
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//void Running(){
+//
+//  
+//}
+//
+//void Idle(){
+//
+//
+//  
+//}
+//
+//void Disabled(){
+//
+//
+//  
+//}
+//
+//void Error(){
+//
+//
+//  
+//}
