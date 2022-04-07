499d8b90e8544efca3ea56fb60a6bc6548db1f62
jcasuga <70498866+jcasuga@users.noreply.github.com>
2022-04-07 12:34:37 -0700

    Implemented water sensor??

--- prototype.ino
+++ prototype.ino
@@ -6,12 +6,16 @@
 
 volatile unsigned char* port_b = (unsigned char*) 0x25; 
 //note to use the pins SDA/SCL pins 20 and 21
+volatile unsigned char* waterPin = (unsigned char*) 0x61;
+volatile unsigned char* powerWaterPin = (unsigned char*) 0x07
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  *powerWaterPin |= 0x01 << 7;
 
 }
 
@@ -20,10 +24,13 @@ void loop() {
 
 }
 
-//void Running(){
-//
-//  
-//}
+int Waterlevel() {
+  int value;
+  *waterPin |= 0x01 << 7;
+  value = analogRead(waterPin);
+  *waterPin &= ~(0x01 << 7); 
+  return value;
+}
 //
 //void Idle(){
 //
