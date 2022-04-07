783f32cb78060bcd8777bbba1ae6eed149cceab4
u4icmailbox <103149621+u4icmailbox@users.noreply.github.com>
2022-04-07 12:42:00 -0700

    Update prototype.ino

--- prototype.ino
+++ prototype.ino
@@ -1,13 +1,13 @@
-#include <DHT.h>
-#include <DHT_U.h>
-#include <RTCZero.h>
-#include <LiquidCrystal.h>
-#include <Stepper.h>
+//#include <DHT.h>
+//#include <DHT_U.h>
+//#include <RTCZero.h>
+//#include <LiquidCrystal.h>
+//#include <Stepper.h>
 
 volatile unsigned char* port_b = (unsigned char*) 0x25; 
 //note to use the pins SDA/SCL pins 20 and 21
 volatile unsigned char* waterPin = (unsigned char*) 0x61;
-volatile unsigned char* powerWaterPin = (unsigned char*) 0x07
+volatile unsigned char* powerWaterPin = (unsigned char*) 0x07;
 
 
 
@@ -21,7 +21,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+Serial.println(Waterlevel());
 }
 
 int Waterlevel() {
